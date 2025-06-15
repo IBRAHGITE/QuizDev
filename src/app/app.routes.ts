98fbfca2fa193b3c1595cd1ffd5bc93bdcf0b020
IBRAHGITE <gite.ibrahim02@gmail.com>
2025-06-15 00:49:38 +0000

    Déploiement GitHub Pages

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,7 +4,7 @@ import { QuizComponent } from './pages/quiz/quiz.component';
 import { ResultatComponent } from './pages/resultat/resultat.component';
 
 export const routes: Routes = [
-  {path:'',component:HomeComponent},
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
   {path:'home',component:HomeComponent},
   {path:'quiz',component:QuizComponent},
   {path:'results',component:ResultatComponent},
